440b9559623af0204715527b3f3e6ca5d16ba0bd
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-15 19:35:45 +1000

    Update StackTraceUtitlity.cs

--- PDBLoaderUNIV/StackTraceUtitlity.cs
+++ PDBLoaderUNIV/StackTraceUtitlity.cs
@@ -1,47 +1,111 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using HarmonyLib;
 using Mono.CompilerServices.SymbolWriter;
+using PDBLoaderUNIV;
 
 public static class StackTracesUtility
 {
-    internal static Dictionary<Assembly, MonoSymbolFile> CachedMDBFiles = new Dictionary<Assembly, MonoSymbolFile>();
-    public static FieldInfo LineNumberInfo = typeof(StackFrame).GetField("lineNumber", AccessTools.all);
-    public static FieldInfo FileNameInfo = typeof(StackFrame).GetField("fileName", AccessTools.all);
+    private static Dictionary<Assembly, MonoSymbolFile> CachedMdbFiles { get; } = new Dictionary<Assembly, MonoSymbolFile>();
+    private static readonly FieldInfo LineNumberInfo = AccessTools.Field(typeof(StackFrame), "lineNumber");
+    private static readonly FieldInfo FileNameInfo = AccessTools.Field(typeof(StackFrame), "fileName");
 
-    internal static bool TryToLoadMonoSymbolFile(Assembly assembly, out MonoSymbolFile symbolFile)
+    private static bool TryToLoadMonoSymbolFile(Assembly assembly, out MonoSymbolFile symbolFile)
     {
-        if (CachedMDBFiles.TryGetValue(assembly, out symbolFile)) return true;
-        string text = Path.ChangeExtension(assembly.Location, ".dll.mdb");
-        if (!File.Exists(text))
+        symbolFile = null;
+    
+        if (assembly == null)
+        {
+            EntryPoint.Log.LogInfo("TryToLoadMonoSymbolFile Failed - Provided assembly is null.");
             return false;
-        FileStream fileStream = File.OpenRead(text);
-        symbolFile = MonoSymbolFile.ReadSymbolFile(fileStream);
-        CachedMDBFiles.Add(assembly, symbolFile);
-        return true;
-			
+        }
+        
+        if (assembly.GetName().Name.Equals("Assembly-CSharp.dll", StringComparison.OrdinalIgnoreCase))
+        {
+            EntryPoint.Log.LogInfo("TryToLoadMonoSymbolFile Skipped - Assembly is Assembly-CSharp.dll.");
+            return false;
+        }
+
+        if (CachedMdbFiles.TryGetValue(assembly, out symbolFile))
+        {
+            return symbolFile != null;
+        }
+
+        var text = Path.ChangeExtension(assembly.Location, ".dll.mdb");
+    
+        if (!File.Exists(text))
+        {
+            EntryPoint.Log.LogInfo($"File does not exist: {text}");
+            return false;
+        }
+
+        try
+        {
+            var fileStream = File.OpenRead(text);
+            symbolFile = MonoSymbolFile.ReadSymbolFile(fileStream);
+            if (symbolFile != null)
+            {
+                CachedMdbFiles.Add(assembly, symbolFile);
+                return true;
+            }
+
+            EntryPoint.Log.LogInfo($"Failed to load symbol file: {text}");
+            return false;
+        }
+        catch (Exception ex)
+        {
+            EntryPoint.Log.LogInfo($"Error occurred during symbol file loading: {ex.Message}");
+            return false;
+        }
     }
 
     public static void ChangeFrame(StackFrame stackFrame)
     {
-        MethodBase methodBase = stackFrame?.GetMethod();
-        if (methodBase == null)
-            return;
-        var tryToLoadMonoSymbolFile = TryToLoadMonoSymbolFile(methodBase.Module.Assembly, out var monoSymbolFile);
-        if (!tryToLoadMonoSymbolFile) return;
-        
-        MethodEntry methodByToken = monoSymbolFile.GetMethodByToken(methodBase.MetadataToken);
-        int iloffset = stackFrame.GetILOffset();
-        foreach (LineNumberEntry lineNumberEntry in methodByToken.GetLineNumberTable().LineNumbers)
+        if (stackFrame == null)
         {
-            if (lineNumberEntry.Offset == iloffset)
+            EntryPoint.Log.LogInfo("ChangeFrame Failed - Provided stackFrame is null.");
+            return;
+        }
+        
+        var methodBase = stackFrame.GetMethod();
+        if (methodBase == null)
+        {
+            EntryPoint.Log.LogInfo("ChangeFrame Failed - MethodBase could not be retrieved from the stackFrame.");
+            return;
+        }
+
+        var assembly = methodBase.Module.Assembly;
+        if (assembly.GetName().Name.Equals("Assembly-CSharp.dll", StringComparison.OrdinalIgnoreCase))
+        {
+            EntryPoint.Log.LogInfo("ChangeFrame Skipped - Assembly is Assembly-CSharp.dll.");
+            return;
+        }
+        
+        if (!TryToLoadMonoSymbolFile(methodBase.Module.Assembly, out var monoSymbolFile) || monoSymbolFile == null)
+        {
+            EntryPoint.Log.LogInfo("Symbol file not found.");
+            return;
+        }
+
+        var methodByToken = monoSymbolFile.GetMethodByToken(methodBase.MetadataToken);
+        if (methodByToken == null)
+        {
+            EntryPoint.Log.LogInfo("ChangeFrame Failed - Method could not be retrieved by token.");
+            return;
+        }
+
+        var offset = stackFrame.GetILOffset();
+        foreach (var lineNumberEntry in methodByToken.GetLineNumberTable().LineNumbers)
+        {
+            if (lineNumberEntry.Offset == offset)
             {
                 LineNumberInfo.SetValue(stackFrame, lineNumberEntry.Row);
                 FileNameInfo.SetValue(stackFrame, methodByToken.CompileUnit.SourceFile.FileName);
+                break;
             }
         }
-        
     }
 }
